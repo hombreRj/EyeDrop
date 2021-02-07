1756b6441824182100f41fdf79bdd8b4e0b1044e
LeonTG <leontg77@gmail.com>
2021-02-07 22:43:13 +0100

    Initial commit

---
+++ src/main/java/lol/leontg/eyedrop/Main.java
@@ -0,0 +1,26 @@
+package lol.leontg.eyedrop;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * The main class of the plugin.
+ *
+ * @author LeonTG
+ */
+public class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void on(PlayerDeathEvent event) {
+        event.getDrops().add(new ItemStack(Material.ENDER_EYE));
+    }
+}
